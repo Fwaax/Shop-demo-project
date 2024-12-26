97ab635b64d6c087ac3a72f034ac9d5735bc3c33
Ryan <ryan32111@walla.co.il>
2024-12-26 16:58:00 +0200

    Minor changes for interfaces

--- server/src/schema/store.ts
+++ server/src/schema/store.ts
@@ -1,9 +1,10 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, ObjectId } from "mongoose";
 
 export interface Store {
-    itemId: string;
+    itemId: ObjectId;
     quantity: number;
     pricePerItem: number;
+    ownerId: ObjectId;
 }
 
 export interface IStoreDocument extends Document { }
@@ -12,6 +13,7 @@ const StoreSchema: Schema = new Schema({
     itemId: { type: Schema.Types.ObjectId, required: true },
     quantity: { type: Number, required: true },
     pricePerItem: { type: Number, required: true },
+    ownerId: { type: Schema.Types.ObjectId, required: true },
 });
 
 export const StoreModel = mongoose.model<IStoreDocument>("Store", StoreSchema);
