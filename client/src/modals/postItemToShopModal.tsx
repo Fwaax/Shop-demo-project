4ea91275f03fffe01de262a32eaa8f68ef24806f
Ryan <ryan32111@walla.co.il>
2024-12-30 12:46:38 +0200

    Props

--- client/src/modals/postItemToShopModal.tsx
+++ client/src/modals/postItemToShopModal.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { cn } from '../utils/classNames';
 import { NumericInputWithNumberValue } from '../components/numberInput';
+import axios from 'axios';
+import { useJwtToken } from '../hooks/useJwtToken';
 
 const PostItemToShopModal = (props: { itemId: string, hide: () => void }) => {
     const [insertedPriceValue, setInsertedPriceValue] = React.useState(0);
     const [insertedQuantityValue, setInsertedQuantityValue] = React.useState(0);
+    const { token } = useJwtToken();
 
     async function handlePostItem() {
         console.log(`insertedPriceValue`, insertedPriceValue);
-
-
+        try {
+            const response = await axios({
+                method: 'post',
+                url: `http://localhost:7821/item/item/${props.itemId}`,  // Fix later not currect url for the posting the item to shop
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                data: { price: insertedPriceValue, quantity: insertedQuantityValue, itemdId: props.itemId },
+            });
+            console.log(`response`, response);
+            return response.data || []; // Ensure response is always defined
+        } catch (error) {
+            console.error("Error fetching my items:", error);
+            return []; // Return empty array on error
+        }
         props.hide();
     }
 
@@ -26,7 +42,7 @@ const PostItemToShopModal = (props: { itemId: string, hide: () => void }) => {
 function TopSection({ hide }: { hide: () => void }) {
     return (
         <div className="flex flex-row justify-between items-center px-4 py-4 border-b border-solid border-white/6">
-            <p className="font-vt323 text-[20px] text-white [word-spacing:6px]">Edit Profile</p>
+            <p className="font-vt323 text-[20px] text-white [word-spacing:6px]">Edit Item</p>
             <span
                 className="flex px-2 rounded-md py-1 border border-solid border-white/10 bg-[#070707] text-white/50 font-vt323 text-md tracking-widest hover:bg-white/10 cursor-pointer"
                 onClick={hide}
@@ -37,19 +53,20 @@ function TopSection({ hide }: { hide: () => void }) {
     );
 }
 
-function BottomSection({
-    hide, onPostBTNClick
-}: {
+function BottomSection(props: {
     hide: () => void;
-    onPostBTNClick: () => void
+    onPostBTNClick: () => void;
 }) {
     return (
         <div className="flex flex-row justify-end gap-4 items-center px-6 py-4 border-t-2 border-solid border-white/6 font-arcade-classic bg-white/2">
+            <div>
+                { }
+            </div>
             <button
                 className={cn(
                     "text-base w-24 justify-center leading-none rounded-[4px] px-6 py-2 text-white/40 bg-white/4 hover:text-white/90 hover:bg-white/10 transition-colors",
                 )}
-                onClick={() => hide()}
+                onClick={() => props.hide()}
             >
                 Cancel
             </button>
@@ -57,7 +74,7 @@ function BottomSection({
                 className={cn(
                     "font-arcade-classic leading-none border-2 border-solid border-[#00CC6A] rounded-[4px] text-[#00CC6A] hover:border-[#00CC6A]/70 hover:text-[#00CC6A]/70 transition-colors flex justify-center items-center px-6 py-2",
                 )}
-                onClick={onPostBTNClick}
+                onClick={props.onPostBTNClick}
             >
                 Post
             </button>
